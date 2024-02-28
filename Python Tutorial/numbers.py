660256386921c8be24e9a8f4b55ef487dc7fa2a1
saktiworkstation <saktiworkstation@gmail.com>
2024-02-28 21:39:13 +0700

    learn about numbers in python

---
+++ Python Tutorial/numbers.py
@@ -0,0 +1,25 @@
+print('# random numbers') # random numbers
+import random
+print(random.randrange(1, 10)) 
+
+print('# type conversion') # type conversion
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
