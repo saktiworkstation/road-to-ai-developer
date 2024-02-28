e0f1a61f21014421775f798a8aa79dd88845fc4e
saktiworkstation <saktiworkstation@gmail.com>
2024-02-28 20:57:50 +0700

    learn data type in python

---
+++ Python Tutorial/data-type.py
@@ -0,0 +1,16 @@
+# Setting the data type
+x = "hello world" # str
+x = 20 # int
+x = 20.5 # float
+x = 20j # complex
+x = ['aple', 'banana', 'cherry'] # list
+x = ('apple', 'banana', 'cherry') # tuple
+x = range(6) # range
+x = {'name': 'sakti', 'age': 19} # dict
+x = {'aple', 'banana', 'cherry'} # set
+x = frozenset({'aple', 'banana', 'cherry'}) #frozenset
+x = True # bool
+x = b'hello world' # bytes
+x = bytearray(5) # bytes array
+x = memoryview(bytes(5)) # memoryview
+x = None # NoneType
