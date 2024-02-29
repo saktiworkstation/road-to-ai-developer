8ed69ce89b10a9dd0470e3b1e5e0ae6e8103a8b7
saktiworkstation <saktiworkstation@gmail.com>
2024-02-29 14:52:30 +0700

    Learn String operation in python

---
+++ Python Tutorial/string.py
@@ -0,0 +1,59 @@
+print('- string are arrays') 
+a = 'hello, world'
+print(a[1])
+
+print('- Looping Through a String') 
+for x in 'banana':
+    print(x)
+
+print('- String Length') 
+a = 'hello, world'
+print(len(a))
+
+print('- Check String') 
+txt = 'the best thing in life are free!'
+print('free' in txt)
+if 'free' in txt:
+    print("yess, 'free' is present")
+
+print('- Check if NOT') 
+txt = 'the best thing in life are free!'
+print('expensive' not in txt)
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.")
+
+print('- Slicing') 
+b = 'hello, world!'
+print(b[1:5])
+
+print('- Clice from the start')
+b = 'hello world!'
+print(b[:5])
+
+print('- Clice from the end')
+b = 'hello world!'
+print(b[2:])
+
+print('- negative indexing')
+b = 'hello world!'
+print(b[-5:-2])
+
+print('- upper case')
+b = 'hello world!'
+print(a.upper())
+
+print('- lower case')
+b = 'Hello World!'
+print(a.lower())
+
+print('- remove whitespace')
+b = ' Hello World!'
+print(a.strip())
+
+print('- replace string')
+b = 'Hello World!'
+print(b.replace('H', 'J'))
+
+print('- split string')
+b = 'Hello, World!'
+print(b.split(','))
