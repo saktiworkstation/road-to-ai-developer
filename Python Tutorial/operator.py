9e2276ebbb6107a19ed89329011d82dab4fc06f5
saktiworkstation <saktiworkstation@gmail.com>
2024-02-29 19:59:05 +0700

    learn operator in python and move the python file

---
+++ Python Tutorial/operator.py
@@ -0,0 +1,16 @@
+print('- operator precedencce')
+print(100 + 5 * 3)
+print((6 + 3) - (6 + 10))
+# ()	Parentheses	
+# **	Exponentiation	
+# +x  -x  ~x	Unary plus, unary minus, and bitwise NOT	
+# *  /  //  %	Multiplication, division, floor division, and modulus	
+# +  -	Addition and subtraction	
+# <<  >>	Bitwise left and right shifts	
+# &	Bitwise AND	
+# ^	Bitwise XOR	
+# |	Bitwise OR	
+# ==  !=  >  >=  <  <=  is  is not  in  not in 	Comparisons, identity, and membership operators	
+# not	Logical NOT	
+# and	AND	
+# or	OR
