bbd6cf55d7ed6cfa3386a6a8ae15b94a0aadd4d2
saktiworkstation <saktiworkstation@gmail.com>
2024-02-29 18:50:20 +0700

    Melanjutkan

--- Python Tutorial/string.py
+++ Python Tutorial/string.py
@@ -57,3 +57,12 @@ print(b.replace('H', 'J'))
 print('- split string')
 b = 'Hello, World!'
 print(b.split(','))
+
+print('- string format')
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "i want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+myorder = "i want {2} pieces of item {1} for {0} dollars."
+print(myorder.format(quantity, itemno, price))
