705edb8fe39b4b7aeaef6d7b54b2431622635254
saktiworkstation <saktiworkstation@gmail.com>
2024-03-02 11:57:16 +0700

    leardn join list

---
+++ Python Tutorial/copy-join-list.py
@@ -0,0 +1,27 @@
+print('- copy list')
+thislist = ['aple', 'banana', 'cherry']
+mylist = thislist.copy()
+print(mylist)
+mylist = list(thislist)
+print(mylist)
+
+print('- join two lists')
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+    list1.append(x)
+
+print(list1)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
