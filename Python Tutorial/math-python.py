f035b7f15e7e79696556a3087a5df23035e9c493
saktiworkstation <saktiworkstation@gmail.com>
2024-03-09 18:26:19 +0700

    learn math using python

---
+++ Python Tutorial/math-python.py
@@ -0,0 +1,22 @@
+print('\n- Math')
+minn = min(5, 10, 25)
+maxx = max(5, 10, 25)
+abss = abs(-7.25)
+poww = pow(4, 3)
+
+print(minn, maxx, abss, poww)
+
+print('\n- The Math Module')
+import math
+
+x = math.sqrt(64)
+print(x)
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+print(x) 
+print(y)
+
+x = math.pi
+
+print(x)
