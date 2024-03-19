81af3f52e5f3fc59bf4286863bb1afb4a401e29f
saktiworkstation <saktiworkstation@gmail.com>
2024-03-19 19:59:41 +0700

    Lern Train\Test

---
+++ Machine Learning/train-test.py
@@ -0,0 +1,121 @@
+import numpy
+import sys
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+
+print("\n- Start With a Data Set")
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+plt.scatter(x, y)
+plt.show()
+
+print("\n- Split Into Train/Test")
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+plt.scatter(test_x, test_y)
+plt.show()
+
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
+print("\n- Display the Testing Set")
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+plt.scatter(test_x, test_y)
+plt.show()
+
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
+print("\n- Fit the Data Set")
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+myline = numpy.linspace(0, 6, 100)
+
+plt.scatter(train_x, train_y)
+plt.plot(myline, mymodel(myline))
+plt.show()
+
+print("\n- R2")
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+r2 = r2_score(train_y, mymodel(train_x))
+
+print(r2)
+
+print("\n- Bring in the Testing Set")
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+r2 = r2_score(test_y, mymodel(test_x))
+
+print(r2)
+
+print("\n- Predict Values")
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+print(mymodel(5))
